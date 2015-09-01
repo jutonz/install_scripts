fe8b01d442cdb35bde27a95e1c4b3b2000f9d8c8
Justin Toniazzo <jutonz42@gmail.com>
2015-08-31 21:13:55 -0400

    Updates ffmpeg if already installed

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install yasm
-git clone git://source.ffmpeg.org/ffmpeg.git ~/.ffmpeg
-cd ~/.ffmpeg
+if [ -d ~/.ffmpeg ]
+then
+  cd ~/.ffmpeg
+  git pull
+else
+  git clone git://source.ffmpeg.org/ffmpeg.git ~/.ffmpeg
+  cd ~/.ffmpeg
+fi
 sudo chmod +x ./configure
 ./configure
 make 
